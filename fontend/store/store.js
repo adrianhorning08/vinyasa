2dd7a776016a890b0310bbf465e3395776999029
Adrian Horning <m.adrian.horning@gmail.com>
2018-01-30 14:50:38 -0800

    Testing the git-ignore

---
+++ fontend/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from '../reducers/root_reducer';
+
+ const configureStore = (preloadedState = {}) => (
+   createStore(
+     rootReducer,
+     preloadedState,
+     composeWithDevTools(applyMiddleware(thunk, logger))
+   )
+ );
+
+ export default configureStore;
