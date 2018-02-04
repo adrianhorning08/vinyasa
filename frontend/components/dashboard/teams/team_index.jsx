08e68a112c5e4a5ad03d354c291bbfca4fe13e85
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-04 10:53:34 -0800

    Ok, NOW the styling for the modal form is done. Had to add an x to close out of it

--- frontend/components/dashboard/teams/team_index.jsx
+++ frontend/components/dashboard/teams/team_index.jsx
@@ -48,6 +48,7 @@ export class TeamIndex extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     this.props.createTeam(this.state);
+    this.setState({name: ''});
     this.closeModal();
   }
 
@@ -83,8 +84,15 @@ export class TeamIndex extends React.Component {
            contentLabel="Example Modal"
          >
          <form onSubmit={this.handleSubmit} className="modal-form">
-           <header className="dialog-header">
+           <header className="modal-form-header">
              New Team
+             <svg onClick={this.closeModal}
+               className="XIcon CloseButton-xIcon"
+               viewBox="0 0 32 32"
+               >
+               <polygon points="24.485,27.314 27.314,24.485 18.828,16 27.314,7.515 24.485,4.686 16,13.172 7.515,4.686 4.686,7.515 13.172,16 4.686,24.485 7.515,27.314 16,18.828">
+               </polygon>
+             </svg>
            </header>
            <label>Name
              <br/>
