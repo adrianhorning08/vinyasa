e3404cf5c67f54740bdfc1d0249d8957bce3773a
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-06 16:36:39 -0800

    Seed data for tasks done

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ User.delete_all
 TeamMembership.delete_all
 ProjectMembership.delete_all
 Project.delete_all
+Task.delete_all
 
 luke = User.create!(
   username: 'Luke Skywalker',
@@ -163,11 +164,6 @@ hoth = Project.create!(
   team_id: rogue.id
 )
 
-attack_death_star = Project.create!(
-  title: 'Attack the Death Star',
-  team_id: rogue.id
-)
-
 dominate = Project.create!(
   title: 'Galatic Domination',
   team_id: empire.id
@@ -216,12 +212,6 @@ rogue.members.each do |member|
     project_id: hoth.id)
 end
 
-rogue.members.each do |member|
-  ProjectMembership.create!(
-    user_id: member.id,
-    project_id: attack_death_star.id)
-end
-
 empire.members.each do |member|
   ProjectMembership.create!(
     user_id: member.id,
@@ -257,3 +247,283 @@ empire.members.each do |member|
     user_id: member.id,
     project_id: teach_stormtroopers.id)
 end
+
+Task.create(
+  title: 'Be the hero',
+  description: '',
+  user_id: luke.id,
+  project_id: blow_up_death_star.id
+)
+
+Task.create(
+  title: 'Use the force',
+  description: 'Do what Ben said',
+  user_id: luke.id,
+  project_id: blow_up_death_star.id
+)
+
+Task.create(
+  title: 'Turn off targeting computer',
+  description: 'Do what Ben said',
+  user_id: luke.id,
+  project_id: blow_up_death_star.id
+)
+
+Task.create(
+  title: 'Bail Luke out',
+  description: 'Do the right thing',
+  user_id: han.id,
+  project_id: blow_up_death_star.id
+)
+
+Task.create(
+  title: 'Watch helplessly',
+  description: '',
+  user_id: leia.id,
+  project_id: blow_up_death_star.id
+)
+
+Task.create(
+  title: 'Be the best wingman in the galaxy',
+  description: '',
+  user_id: wedge.id,
+  project_id: blow_up_death_star.id
+)
+
+Task.create(
+  title: 'Take down the empire...single handedly',
+  description: '',
+  user_id: luke.id,
+  project_id: peace.id
+)
+
+Task.create(
+  title: 'Betray Han',
+  description: '',
+  user_id: lando.id,
+  project_id: peace.id
+)
+
+Task.create(
+  title: 'Save Han',
+  description: '',
+  user_id: lando.id,
+  project_id: peace.id
+)
+
+Task.create(
+  title: 'Apologize to Han',
+  description: '',
+  user_id: lando.id,
+  project_id: peace.id
+)
+
+Task.create(
+  title: 'Blow up the walkers',
+  description: '',
+  user_id: luke.id,
+  project_id: hoth.id
+)
+
+Task.create(
+  title: 'Blow up the walkers',
+  description: '',
+  user_id: wedge.id,
+  project_id: hoth.id
+)
+
+Task.create(
+  title: 'Blow up the walkers',
+  description: '',
+  user_id: wedge.id,
+  project_id: hoth.id
+)
+
+Task.create(
+  title: 'Kill all my commanders',
+  description: '',
+  user_id: vader.id,
+  project_id: dominate.id
+)
+
+Task.create(
+  title: 'Just let vader do everything',
+  description: '',
+  user_id: palpatine.id,
+  project_id: dominate.id
+)
+
+Task.create(
+  title: 'Get a face lift',
+  description: '',
+  user_id: palpatine.id,
+  project_id: dominate.id
+)
+
+Task.create(
+  title: 'Blow up all the planets with the death star',
+  description: '',
+  user_id: palpatine.id,
+  project_id: dominate.id
+)
+
+Task.create(
+  title: 'Intimidate the hell outta people',
+  description: '',
+  user_id: vader.id,
+  project_id: dominate.id
+)
+
+Task.create(
+  title: 'Find luke',
+  description: '',
+  user_id: obi_wan.id,
+  project_id: destroy_sith.id
+)
+
+Task.create(
+  title: 'Fight Vader',
+  description: '',
+  user_id: obi_wan.id,
+  project_id: destroy_sith.id
+)
+
+Task.create(
+  title: 'Train Luke',
+  description: '',
+  user_id: yoda.id,
+  project_id: destroy_sith.id
+)
+
+Task.create(
+  title: 'Fight Vader',
+  description: '',
+  user_id: luke.id,
+  project_id: destroy_sith.id
+)
+
+Task.create(
+  title: 'Let dad kill the emperor',
+  description: '',
+  user_id: luke.id,
+  project_id: destroy_sith.id
+)
+
+Task.create(
+  title: 'Say very inspirational and deep things to Luke',
+  description: '',
+  user_id: yoda.id,
+  project_id: train_luke.id
+)
+
+Task.create(
+  title: 'Teach Luke to move rocks with his mind',
+  description: '',
+  user_id: yoda.id,
+  project_id: train_luke.id
+)
+
+Task.create(
+  title: 'Come off as a crazy old man',
+  description: '',
+  user_id: yoda.id,
+  project_id: train_luke.id
+)
+
+Task.create(
+  title: "Get Luke's ship out",
+  description: 'Stupid kid',
+  user_id: yoda.id,
+  project_id: train_luke.id
+)
+
+Task.create(
+  title: "Get Luke out of Tatooine",
+  description: '',
+  user_id: obi_wan.id,
+  project_id: train_luke.id
+)
+
+Task.create(
+  title: "Save Luke's life",
+  description: '',
+  user_id: obi_wan.id,
+  project_id: train_luke.id
+)
+
+Task.create(
+  title: "Try not to get kil...too late",
+  description: '',
+  user_id: obi_wan.id,
+  project_id: train_luke.id
+)
+
+Task.create(
+  title: "Kill Obi-Wan",
+  description: '',
+  user_id: vader.id,
+  project_id: destroy_jedi.id
+)
+
+Task.create(
+  title: "Convert Luke",
+  description: '',
+  user_id: vader.id,
+  project_id: destroy_jedi.id
+)
+
+Task.create(
+  title: "Kill Luke??",
+  description: '',
+  user_id: vader.id,
+  project_id: destroy_jedi.id
+)
+
+Task.create(
+  title: "Let Vader do everything",
+  description: '',
+  user_id: palpatine.id,
+  project_id: destroy_jedi.id
+)
+
+Task.create(
+  title: "Zap Luke...a lot",
+  description: '',
+  user_id: palpatine.id,
+  project_id: convert_luke.id
+)
+
+Task.create(
+  title: "Tell Vader to convert Luke",
+  description: '',
+  user_id: palpatine.id,
+  project_id: convert_luke.id
+)
+
+Task.create(
+  title: "Freeze Luke in carbonite",
+  description: '',
+  user_id: vader.id,
+  project_id: convert_luke.id
+)
+
+Task.create(
+  title: "Tell Luke I'm his dad",
+  description: '',
+  user_id: vader.id,
+  project_id: convert_luke.id
+)
+
+Task.create(
+  title: "Target practice",
+  description: '',
+  user_id: vader.id,
+  project_id: teach_stormtroopers.id
+)
+
+Task.create(
+  title: "Get new clones",
+  description: '',
+  user_id: tarkin.id,
+  project_id: teach_stormtroopers.id
+)
