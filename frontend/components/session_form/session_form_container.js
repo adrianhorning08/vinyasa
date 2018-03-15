74322d9dd32ccc4b9eab69b0079719e70c699cab
Adrian Horning <m.adrian.horning@gmail.com>
2018-03-15 14:56:43 -0700

    Cleaned up some comments

--- frontend/components/session_form/session_form_container.js
+++ frontend/components/session_form/session_form_container.js
@@ -2,14 +2,7 @@ import { connect } from 'react-redux';
 import { SessionForm } from './session_form';
 import { login, signup, clearErrors } from '../../actions/session_actions';
 
-// Why don't we need withRouter here? Didn't we need it in the
-// practice test when we were trying to render a different form?
-
 const mapStateToProps = (state, ownProps) => {
-  // loggedIn (boolean) - representing whether a currentUser exists
-  // errors (array) - list of errors from the state
-  // formType (string): 'login' or 'signup' given the current
-  // ownProps.location.pathname
   let loggedIn = state.session.currentUser ? true : false;
   let errors = state.errors.session;
   let formType = ownProps.match.path === '/login' ? 'login' : 'signup';
