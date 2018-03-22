5da11187f607490fd3010a249037a7533cc52c3e
Adrian Horning <m.adrian.horning@gmail.com>
2018-03-22 16:33:39 -0700

    Text up

--- frontend/components/landing.jsx
+++ frontend/components/landing.jsx
@@ -10,11 +10,21 @@ export class Landing extends React.Component {
       <div className="landing">
         <header className="lading-header">
           <div className="header-logo">
-
           </div>
         </header>
         <section className="masthead">
-
+          <div className="hero-container">
+            <h1>Move work</h1>
+            <h1>forward</h1>
+            <div className="hero-subtext">
+              <p>Vinyasa is the easiest way for teams to</p>
+              <br/>
+              <p>track their work - and get results.</p>
+            </div>
+            <div className="get-started-hero">
+              <button>Get Started</button>
+            </div>
+          </div>
         </section>
       </div>
     );
